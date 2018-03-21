4008a59a999e695e7556c001148adc2239ce6997
Christian Thing Knudsen <christian.thing.knudsen@post.au.dk>
2018-03-21 11:49:29 +0100

    IT2

---
+++ MicrowaveOven.Test.Integration/IT2_UserInterface_Door.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    class IT2_UserInterface_Door
+    {
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _startCancelButton;
+        private IDoor _door;
+        private ILight _light;
+        private IDisplay _display;
+        private ICookController _cookController;
+        private UserInterface _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+
+            _door = Substitute.For<IDoor>();
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+            _cookController = Substitute.For<ICookController>();
+
+            _uut = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+        }
+
+
+    }
+}
